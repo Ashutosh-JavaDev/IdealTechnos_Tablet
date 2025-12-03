a42b79d8b2cc6c682c4250d828af68aaffe69c95
Ashutosh Ayush <jbnashu297@gmail.com>
2025-12-03 11:31:29 +0530

    Java Swing Project

---
+++ IdealProject/src/Main.java
@@ -0,0 +1,11 @@
+import javax.swing.*;
+import java.awt.Color;
+import java.awt.Font;
+import java.awt.ActiveEvent;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+public class Main {
+    public static void main(String[] args) throws Exception {
+        System.out.println("Hello, World!");
+    }
+}
